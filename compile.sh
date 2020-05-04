4a39ad010a770e43eaae0c202ca51f69a2a3432e
gaby.santamaria@outlook.fr <gaby.santamaria@outlook.fr>
2020-05-04 20:59:27 +0200

    modified shell script

--- compile.sh
+++ compile.sh
@@ -15,11 +15,31 @@ function is_ocaml_installed ()
     fi
 }
 
+# This function will just try to launch a command
+function try ()
+{
+    local value=$1
+    local err
+    err="$(eval $value)" 1>&1 || {
+        echo "[Compile.sh] An error occurred."
+        echo "[Compile.sh] Warning, I'll give you the error very soon..."
+        sleep 1
+        echo "[Compile.sh] Ok, actually I have to... check... if I have my helmet..."
+        sleep 1
+        echo "[Compile.sh] Alright everything is O.K, here's the error:"
+        echo "[Compile.sh] ERROR: ${err}"
+
+        false
+
+        exit 1
+    }
+}
+
 # Basically change the directory to "src"
 # $1 is the location of the Brainfuck's git repository
 function go_to_src ()
 {
-    cd "$1" . "/src" || echo "An error occurred while trying to look-up Brainfuck/src"
+    try "cd ${1}\"src\"" || echo "An error occurred while trying to look-up Brainfuck/src"
 }
 
 # Compile the given file
@@ -28,8 +48,8 @@ function compile_module ()
 {
     local file_names=$1
     for i in $file_names; do
-        ocamlopt -c "$i" . ".mli"
-        ocamlopt -c "$i" . ".ml"
+        try "ocamlopt -c \"${i}.mli\""
+        try "ocamlopt -c \"${i}.ml\""
     done
 }
 
@@ -38,17 +58,26 @@ function compile ()
 {
     local dependencies=$1
     local modules=$2
-    ocamlopt dependencies -o "brainfuck" modules
+    try "ocamlopt ${dependencies} -o \"brainfuck\" ${modules}"
 }
 
 function main ()
 {
-    if [ ! "$(is_ocaml_installed)" ]; then # check if ocamlopt is available to compile Brainfuck
-        echo "ocamlopt isn't installed. Can't compile Brainfuck. Sorry mate."
+    local default=""
+    if [ "$#" -ne 1 ]; then
+        ${1:-$default}
+        echo "[Compile.sh] OKay darling, you're already in the right folder"
+    fi
+
+    is_ocaml_installed
+    if [ ! "$?" ]; then # check if ocamlopt is available to compile Brainfuck
+        echo "[Compile.sh] Hoooo! No! \"ocamlopt\" isn't installed. Can't compile Brainfuck."
+        echo "[Compile.sh] - Sorry mate."
+        echo "[Compile.sh] Try to install OCaml using: 'apt install ocaml'"
         exit 1
     fi
 
-    go_to_src "$1"
+    go_to_src $1
 
     modules=(
         "util"
@@ -62,11 +91,14 @@ function main ()
         "str"
     )
 
-    echo "Compiling modules..."
+    echo "[Compile.sh] Compiling modules..."
     compile_module "${modules[*]}"
-    echo "Compiling"
+    echo "[Compile.sh] Compiling"
+    sleep 1
     compile "brainfuck_c" "${dependencies[*]}" "${modules[*]}"
-    echo "Ended."
+    echo "[Compile.sh] Ended."
+    sleep 1
+    echo "[Compile.sh] I hope everything was alright whith me. Please, buy me a coffee, I'm so tired helping people to compile all the day long..."
 }
 
-main
+main $1
